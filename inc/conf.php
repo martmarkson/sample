b3351e1807a2a7f16a0c7c78a2a94493cc8ba932
Märt Märkson <Mart.Markson@ltk.local>
2019-10-14 10:14:41 +0300

    Revert "modified conf"
    This reverts commit 26479d767ccce2a7e76398d5c9c199a752bf7d7e.

--- inc/conf.php
+++ inc/conf.php
@@ -12,5 +12,4 @@ ini_set('memory_limit', '64M');
 // Main constants
 define('SITEURL', 'www.somedomainname.com');
 define('GENERALMAIL', 'info@somedomainname.com');
-define('LASTMOD', 'lastmodified');
 ?>
